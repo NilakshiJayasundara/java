8c3cf62d3feb07d1f3b880b292bd57f7feb0db5f
NilakshiJayasundara <nilamjayasundara@gmail.com>
2019-05-29 13:12:46 +0530

    Third commit

---
+++ JavaApplication3/src/javaapplication3/statement.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication3;
+
+/**
+ *
+ * @author nilakshi
+ */
+public class statement {
+    public void marks(){
+        
+        int mark= 69;
+        if(mark<=100 && mark>=90){
+         System.out.println("A");
+        }
+         else if(mark<=90 && mark>=80){
+                 System.out.println("B");
+         }
+         else if(mark<=80 && mark>=70){
+            System.out.println("C");
+         }
+         else{
+         System.out.println("D");
+         }
+        }
+            
+                
+    }
+   
